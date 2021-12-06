53ce07ec9e37b16bae7550cfe0e646464e27d303
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-06 13:39:40 +0000

    add pre-commit hook

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import {storeData} from "./utils/config"
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>NBC Meetup</Text>
-      <Button onPress={()=>{storeData()}}
-          title="Click Me"
-          color="#841584"/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  StyleSheet, Text, View, Button,
+} from 'react-native';
+import { storeData } from './utils/config';
 
 const styles = StyleSheet.create({
   container: {
@@ -23,3 +13,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>NBC Meetup</Text>
+      <Button onPress={() => { storeData(); }}
+          title="Click Me"
+          color="#841584"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
