7ab282e0962f2a4d0cd51324337d7494193386fc
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-08 16:51:20 +0000

    add addNewEvent, deleteUser, deleteEvent

--- utils/utils.js
+++ utils/utils.js
@@ -1,83 +1,89 @@
-import { db } from "./firestoreConfig.js";
-import { collection, getDocs, getDoc, query, where, doc, setDoc, addDoc, createId } from "firebase/firestore";
-
+import { db } from './firestoreConfig.js'
+import {
+  collection,
+  getDocs,
+  getDoc,
+  query,
+  where,
+  doc,
+  addDoc,
+  deleteDoc
+} from 'firebase/firestore'
 
 export const selectAllEvents = () => {
-  return getDocs(collection(db, "events"))
+  return getDocs(collection(db, 'events'))
     .then((snapshot) => {
-      let eventsArray = [];
+      let eventsArray = []
       snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
+        eventsArray.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(eventsArray)
       return eventsArray
     })
     .catch((err) => {
-      console.log(err);
-    });
-};
+      console.log(err)
+    })
+}
 
-export const selectEventsByUser = (user_id) => {
-  const q = query(collection(db, "events"), where("host_id", "==", user_id));
+export const selectEventsByUser = (userId) => {
+  const q = query(collection(db, 'events'), where('host_id', '==', userId))
   return getDocs(q)
     .then((snapshot) => {
-      let eventsArray = [];
+      let eventsArray = []
       snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
+        eventsArray.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(eventsArray)
       return eventsArray
     })
     .catch((err) => {
-      console.log(err);
-    });
-};
+      console.log(err)
+    })
+}
 
 export const selectEventById = (eventId) => {
-  const docRef = doc(db, "events", eventId);
+  const docRef = doc(db, 'events', eventId)
   return getDoc(docRef).then((snapshot) => {
     const event = snapshot.data()
     console.log(event)
     return event
-});
+  })
 }
-// MVP just leaving it as one filter instead of complex. 
+
+export const addNewEvent = (newEvent) => {
+  return addDoc(collection(db, 'events'), newEvent).then((res) => {
+    console.log(res.id)
+    return res.id
+  })
+}
+
+// MVP just leaving it as one filter instead of complex.
 export const selectFilteredEvents = (location) => {
-  const q1 = query(collection(db, "events"), where("location", "==", location));
+  const q1 = query(collection(db, 'events'), where('location', '==', location))
   return getDocs(q1)
     .then((snapshot) => {
-      let eventsArray = [];
+      let eventsArray = []
       snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
+        eventsArray.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(eventsArray)
       return eventsArray
     })
     .catch((err) => {
-      console.log(err);
-    });
-};
-
-
-
-
-// This is not getting into the promise chain
-
-export const addNewEvent = (newEvent) => {
-  console.log("ASOMETHINGSDG")
-
-  return addDoc(collection(db, "events"), newEvent)
-    .then((result) => {
-      console.log("Success")
-      console.log(result)
-      return newEvent.id
+      console.log(err)
     })
 }
 
+export const deleteUser = (userId) => {
+  return deleteDoc(doc(db, "users", userId))
+  .then(() => {
+    console.log("user deleted!")
+  })
+}
 
-
-
-
-// Filtered selection of all events
-
-// Delete functionality
+export const deleteEvent = (eventId) => {
+  return deleteDoc(doc(db, "events", eventId))
+  .then(() => {
+    console.log("event deleted!")
+  })
+}
