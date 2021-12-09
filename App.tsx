cf9f7b81971caa3e7889618329b05429e5a16b71
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-09 19:07:30 +0000

    change userExist state name

--- App.tsx
+++ App.tsx
@@ -1,27 +1,29 @@
-import React, {useState} from "react";
-import { Nav } from "./components/Nav.view/Nav.component";
-import { firebaseApp } from "./utils/firestoreConfig";
-import { getAuth } from "@firebase/auth";
-import { SplashScreen } from "./components/Splash.screen/splash.component";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { useState } from 'react'
+import { Nav } from './components/Nav.view/Nav.component'
+import { firebaseApp } from './utils/firestoreConfig'
+import { getAuth } from '@firebase/auth'
+import { SplashScreen } from './components/Splash.screen/splash.component'
+import { NavigationContainer } from '@react-navigation/native'
 
-const auth = getAuth();
+const auth = getAuth()
 
 export default function App() {
   firebaseApp
-  
-    const [currentUser, setCurrentUser] = useState(false)
-  
-    auth.onAuthStateChanged((user) =>{
-      if (user) {
-        setCurrentUser(true)
-      }
-    });
-  
-  if(!currentUser){return(<SplashScreen />)}
+
+  const [userExist, setUserExist] = useState(false)
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUserExist(true)
+    }
+  })
+
+  if (!userExist) {
+    return <SplashScreen />
+  }
   return (
     <NavigationContainer>
       <Nav />
     </NavigationContainer>
-  );
+  )
 }
--- components/Splash.screen/splash.component.tsx
+++ components/Splash.screen/splash.component.tsx
@@ -1,26 +1,28 @@
-import React, {useState} from "react";
-import { Text, View } from "react-native";
-import { getAuth } from 'firebase/auth';
-import { PhoneSignIn } from "../Auth.screen/Auth.component";
-import { firebaseApp } from "../../utils/firestoreConfig"
+import React, { useState } from 'react'
+import { Text, View } from 'react-native'
+import { getAuth } from 'firebase/auth'
+import { PhoneSignIn } from '../Auth.screen/Auth.component'
+import { firebaseApp } from '../../utils/firestoreConfig'
 
-const auth = getAuth();
+const auth = getAuth()
 
 export const SplashScreen = () => {
-    firebaseApp
+  firebaseApp
 
-    const [currentUser, setCurrentUser] = useState(false)
-  
-    auth.onAuthStateChanged((user) =>{
-      if (user) {
-        setCurrentUser(true)
-      }
-    });
-  
-    if(!currentUser){return(<PhoneSignIn/>)}
-    return(
-        <View>
-          <Text>Welcome to NBC</Text>
-        </View>
-    )
+  const [userExist, setUserExist] = useState(false)
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUserExist(true)
+    }
+  })
+
+  if (!userExist) {
+    return <PhoneSignIn />
+  }
+  return (
+    <View>
+      <Text>Welcome to NBC</Text>
+    </View>
+  )
 }
