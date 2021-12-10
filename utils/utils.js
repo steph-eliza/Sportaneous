4a7ef1438bffd81ab44d6ee1e3f4711d9c835d50
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-10 21:31:27 +0000

    add AddEvent comp

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-import { db } from "./firestoreConfig.js";
+import { db } from './firestoreConfig.js'
 import {
   collection,
   getDocs,
@@ -12,154 +12,113 @@ import {
   arrayUnion,
   arrayRemove,
   setDoc,
-} from "firebase/firestore";
+} from 'firebase/firestore'
 
 export const selectAllEvents = () => {
-  return getDocs(collection(db, "events"))
-    .then((snapshot) => {
-      let eventsArray = [];
-      snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
-      return eventsArray;
+  return getDocs(collection(db, 'events')).then((snapshot) => {
+    let eventsArray = []
+    snapshot.docs.forEach((doc) => {
+      eventsArray.push({ ...doc.data(), id: doc.id })
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+    return eventsArray
+  })
+}
 
 export const selectEventsByUser = (userId) => {
-  const q = query(collection(db, "events"), where("host_id", "==", userId));
-  return getDocs(q)
-    .then((snapshot) => {
-      let eventsArray = [];
-      snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
-      return eventsArray;
+  const q = query(collection(db, 'events'), where('host_id', '==', userId))
+  return getDocs(q).then((snapshot) => {
+    let eventsArray = []
+    snapshot.docs.forEach((doc) => {
+      eventsArray.push({ ...doc.data(), id: doc.id })
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+    return eventsArray
+  })
+}
 
 export const selectEventById = (eventId) => {
-  const docRef = doc(db, "events", eventId);
+  const docRef = doc(db, 'events', eventId)
   return getDoc(docRef).then((snapshot) => {
-    const event = snapshot.data();
-    console.log(event);
-    return event;
-  });
-};
+    const event = snapshot.data()
+    return event
+  })
+}
 
 export const addNewEvent = (newEvent) => {
-  return addDoc(collection(db, "events"), newEvent).then((res) => {
-    console.log(res.id);
-    return res.id;
-  });
-};
+  return addDoc(collection(db, 'events'), newEvent).then((res) => res.id)
+}
 
 export const addNewUser = (newUser, uid) => {
-  return setDoc(doc(db, "users", uid), newUser);
-};
+  return setDoc(doc(db, 'users', uid), newUser)
+}
 
-export const addNewChatroom = (newChatroom) => {
-  return addDoc(collection(db, "chats"), newChatroom).then((res) => {
-    console.log(res.id);
-    return res.id;
-  });
-};
+export const addNewChatroom = (newChatroom, eventId) => {
+  return setDoc(doc(db, 'chats', eventId), newChatroom)
+}
 
 // MVP just leaving it as one filter instead of complex.
 export const selectFilteredEvents = (location) => {
-  const q1 = query(collection(db, "events"), where("location", "==", location));
-  return getDocs(q1)
-    .then((snapshot) => {
-      let eventsArray = [];
-      snapshot.docs.forEach((doc) => {
-        eventsArray.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(eventsArray);
-      return eventsArray;
+  const q1 = query(collection(db, 'events'), where('location', '==', location))
+  return getDocs(q1).then((snapshot) => {
+    let eventsArray = []
+    snapshot.docs.forEach((doc) => {
+      eventsArray.push({ ...doc.data(), id: doc.id })
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+    return eventsArray
+  })
+}
 
 export const deleteUser = (userId) => {
-  return deleteDoc(doc(db, "users", userId)).then(() => {
-    console.log("user deleted!");
-  });
-};
+  return deleteDoc(doc(db, 'users', userId))
+}
 
 export const deleteEvent = (eventId) => {
-  return deleteDoc(doc(db, "events", eventId)).then(() => {
-    console.log("event deleted!");
-  });
-};
+  return deleteDoc(doc(db, 'events', eventId))
+}
 
 export const deleteChatroom = (chatId) => {
-  return deleteDoc(doc(db, "chats", chatId)).then(() => {
-    console.log("chatroom deleted!");
-  });
-};
+  return deleteDoc(doc(db, 'chats', chatId))
+}
 
 export const getUsers = () => {
-  return getDocs(collection(db, "users"))
-    .then((snapshot) => {
-      let usersArray = [];
-      snapshot.docs.forEach((user) => {
-        usersArray.push({ ...user.data(), id: user.id });
-      });
-      console.log(usersArray);
-      return usersArray;
+  return getDocs(collection(db, 'users')).then((snapshot) => {
+    let usersArray = []
+    snapshot.docs.forEach((user) => {
+      usersArray.push({ ...user.data(), id: user.id })
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+    return usersArray
+  })
+}
 
 export const getUserById = (userId) => {
-  const docRef = doc(db, "users", userId);
+  const docRef = doc(db, 'users', userId)
   return getDoc(docRef).then((snapshot) => {
-    const user = snapshot.data();
-    console.log(user);
-    return user;
-  });
-};
+    const user = snapshot.data()
+    return user
+  })
+}
 
 export const selectChatById = (chatId) => {
-  const docRef = doc(db, "chats", chatId);
+  const docRef = doc(db, 'chats', chatId)
   return getDoc(docRef).then((snapshot) => {
-    const event = snapshot.data();
-    console.log("event:", event);
-    return event;
-  });
-};
+    const event = snapshot.data()
+    return event
+  })
+}
 
 export const addChatMessage = (chatObject, chatId) => {
-  return updateDoc(doc(db, "chats", chatId), {
+  return updateDoc(doc(db, 'chats', chatId), {
     messages: arrayUnion(chatObject),
-  }).then((res) => {
-    console.log(res);
-  });
-};
+  })
+}
 
 export const deleteChatMessage = (chatObject, chatId) => {
-  return updateDoc(doc(db, "chats", chatId), {
+  return updateDoc(doc(db, 'chats', chatId), {
     messages: arrayRemove(chatObject),
-  }).then((res) => {
-    console.log(res);
-  });
-};
+  })
+}
 
 export const joinEvent = (userDetails, eventId) => {
-  return updateDoc(doc(db, "events", eventId), {
+  return updateDoc(doc(db, 'events', eventId), {
     pending_attendees: arrayUnion(userDetails),
-  }).then((res) => {
-    console.log(res);
-  });
-};
+  })
+}
