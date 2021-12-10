04d1d39a10158ba23fb3421aa6dca33fa7e095ec
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-10 21:42:05 +0000

    add AddEvent navigation

--- components/Nav.view/Nav.component.tsx
+++ components/Nav.view/Nav.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { SingleEvent } from "../SingleEvent.screen/SingleEvent.component";
+import { AddEvent } from "../AddEvent.screen/AddEvent.component";
 import EventList from "../Events.screen/EventList.component";
 import PhoneSignIn from "../Auth.screen/Auth.component";
 import Chatroom from "../Chatroom.screen/Chatroom.component";
@@ -13,6 +14,7 @@ export function Nav() {
   return (
     <Drawer.Navigator initialRouteName="EventList">
       <Drawer.Screen name="EventList" component={EventList} />
+      <Drawer.Screen name="Add Event" component={AddEvent} />
       <Drawer.Screen name="Event" component={SingleEvent} options={{drawerItemStyle:{display:"none"}}}/>
       <Drawer.Screen name="PhoneSignIn" component={PhoneSignIn} options={{drawerItemStyle:{display:"none"}}}/>
       <Drawer.Screen name="GetUserName" component={GetUserName} options={{drawerItemStyle:{display:"none"}}}/>
