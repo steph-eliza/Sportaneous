ac300e48cd6b910adc955d3f454ddef7124eda4a
WillWatkins <w.watkins@hotmail.co.uk>
2021-12-13 14:55:38 +0000

    Refactor join/leave event button to be dynamic

--- components/SingleEvent.screen/SingleEvent.component.tsx
+++ components/SingleEvent.screen/SingleEvent.component.tsx
@@ -1,7 +1,13 @@
 import { View, Text, Pressable } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { styles } from "./SingleEvent.style";
-import { getUserById, joinEvent, selectEventById } from "../../utils/utils";
+import { UserContext } from "../../contexts/UserContext";
+import {
+  getUserById,
+  joinEvent,
+  removeSelfFromEvent,
+  selectEventById,
+} from "../../utils/utils";
 
 type AddEventProps = {
   navigation: {
@@ -14,10 +20,9 @@ type AddEventProps = {
 
 export const SingleEvent = ({ navigation, route }: AddEventProps) => {
   const { eventId } = route.params;
-  const user = { userId: "1234", first_name: "Will", second_name: "test" };
+  const { currentUser } = useContext(UserContext);
 
-  const [isLoading, setIsLoading] = React.useState(true);
-  //Dummy object to shut up TS.
+  const [attendingStatus, setAttendingStatus] = React.useState(false);
   const [eventDetails, setEventDetails] = React.useState({
     attendees: [""],
     category: "Dummy",
@@ -26,21 +31,33 @@ export const SingleEvent = ({ navigation, route }: AddEventProps) => {
     host_id: "Dummmy",
     location: "Dummmy",
     max_capacity: 4,
-    pending_attendees: [],
+    pending_attendees: [""],
     title: "Dummmy",
     id: 123,
+    time: "",
   });
   const [hostDetails, setHostDetails] = React.useState({
     first_name: "",
     last_name: "",
   });
 
+  const checkAttendingStatus = (object: any) => {
+    if (object.attendees.includes(currentUser.id)) {
+      setAttendingStatus(true);
+    }
+
+    for (let i = 0; i < object.pending_attendees.length; i++) {
+      if (object.pending_attendees[i].userId === currentUser) {
+        setAttendingStatus(true);
+      }
+    }
+  };
+
   React.useEffect(() => {
-    setIsLoading(true);
     selectEventById(eventId)
       .then((event: any) => {
         setEventDetails(event);
-        setIsLoading(false);
+        checkAttendingStatus(event);
         return event;
       })
       .then((host) => {
@@ -54,52 +71,57 @@ export const SingleEvent = ({ navigation, route }: AddEventProps) => {
       });
   }, [eventId]);
 
-  if (isLoading) {
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    );
-  } else {
-    return (
-      <>
-        <View style={styles.view}>
-          <Text style={styles.title}>{eventDetails.title}</Text>
-          <Text style={styles.text}>Location: {eventDetails.location}</Text>
-          <Text style={styles.text}>Category: {eventDetails.category}</Text>
-          <Text style={styles.text}>
-            Description: {eventDetails.description}
-          </Text>
-          <Text style={styles.text}>Date: {eventDetails.date}</Text>
-          <Text style={styles.text}>
-            Places: {eventDetails.attendees.length}/{eventDetails.max_capacity}
-          </Text>
-          <Pressable
-            style={styles.pressable}
-            onPress={() => {
-              joinEvent(user, eventId);
-            }}
-          >
-            <Text style={styles.PressableText}>Join this event?</Text>
-          </Pressable>
-        </View>
-        <View style={styles.view}>
-          <Text style={styles.text}>Event host:</Text>
-          <Text
-            style={styles.text}
-          >{`${hostDetails.first_name} ${hostDetails.last_name}`}</Text>
+  const userDetailsForEvent = {
+    first_name: currentUser.first_name,
+    last_name: currentUser.last_name,
+    userId: currentUser.id,
+  };
 
-          <Pressable
-            onPress={() => {
-              //Navigate back to EventsList when added to repo
-              navigation.navigate("Events");
-            }}
-            style={styles.pressable}
-          >
-            <Text style={styles.PressableText}>Go back to events</Text>
-          </Pressable>
-        </View>
-      </>
-    );
-  }
+  return (
+    <>
+      <View style={styles.view}>
+        <Text style={styles.title}>{eventDetails.title}</Text>
+        <Text style={styles.text}>Location: {eventDetails.location}</Text>
+        <Text style={styles.text}>Category: {eventDetails.category}</Text>
+        <Text style={styles.text}>Description: {eventDetails.description}</Text>
+        <Text style={styles.text}>{`Time: ${eventDetails.time}`}</Text>
+        <Text style={styles.text}>{`Date: ${eventDetails.date}`}</Text>
+        <Text style={styles.text}>
+          Places: {eventDetails.attendees.length}/{eventDetails.max_capacity}
+        </Text>
+        <Pressable
+          style={styles.pressable}
+          onPress={() => {
+            if (!attendingStatus) {
+              setAttendingStatus(true);
+              joinEvent(userDetailsForEvent, eventId);
+            } else {
+              //leaveEvent api call
+              setAttendingStatus(false);
+              removeSelfFromEvent(userDetailsForEvent, eventId);
+            }
+          }}
+        >
+          <Text style={styles.PressableText}>
+            {attendingStatus ? "Leave event?" : "Join event?"}
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.text}>Event host:</Text>
+        <Text
+          style={styles.text}
+        >{`${hostDetails.first_name} ${hostDetails.last_name}`}</Text>
+
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Events");
+          }}
+          style={styles.pressable}
+        >
+          <Text style={styles.PressableText}>Go back to events</Text>
+        </Pressable>
+      </View>
+    </>
+  );
 };
