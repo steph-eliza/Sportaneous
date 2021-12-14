2d1f989c73edf6db52c40d9c79b5bd1c72e1ddb9
WillWatkins <w.watkins@hotmail.co.uk>
2021-12-14 20:48:00 +0000

    Extract out hostDetails type declaration into utils

--- components/SingleEvent.screen/subcomponents/HostInfo/HostInfo.component.tsx
+++ components/SingleEvent.screen/subcomponents/HostInfo/HostInfo.component.tsx
@@ -1,22 +1,16 @@
 import { View, Text } from "react-native";
 import { styles } from "./HostInfo.style";
 import React from "react";
+import { addEventProps } from "../../singleEvent.utils";
 
-type hostDetailsType = {
-  first_name: string;
-  last_name: string;
-  description: string;
-  image_bitmap: string;
-};
-
-export const HostInfo = (hostDetails: hostDetailsType) => {
+export const HostInfo = ({ hostDetails }: addEventProps) => {
   return (
     <View style={styles.hostView}>
       <Text style={styles.text}>About the host:</Text>
       <Text
         style={styles.text}
-      >{`${hostDetails.first_name} ${hostDetails.last_name}`}</Text>
-      <Text style={styles.text}>{hostDetails.description}</Text>
+      >{`${hostDetails?.first_name} ${hostDetails?.last_name}`}</Text>
+      <Text style={styles.text}>{hostDetails?.description}</Text>
     </View>
   );
 };
